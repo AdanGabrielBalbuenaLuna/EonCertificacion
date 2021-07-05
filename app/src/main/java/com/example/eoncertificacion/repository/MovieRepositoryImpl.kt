cf724efc6ed6265a5ed0f002ff8cc9148e5d8e05
Gabriel <agbalbuenaluna@gmail.com>
2021-07-05 13:21:05 -0500

    Datasource on MovieRepositoryImpl.kt

--- app/src/main/java/com/example/eoncertificacion/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/eoncertificacion/repository/MovieRepositoryImpl.kt
@@ -1,18 +1,11 @@
 package com.example.eoncertificacion.repository
 
 import com.example.eoncertificacion.data.model.MovieList
+import com.example.eoncertificacion.data.remote.MovieDataSource
 
-class MovieRepositoryImpl:MovieRepository{
-    override suspend fun getUpcomingMovies(): MovieList {
-        TODO("Not yet implemented")
-    }
+class MovieRepositoryImpl(private val dataSource: MovieDataSource): MovieRepository {
 
-    override suspend fun getTopRatedMovies(): MovieList {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getPopularMovies(): MovieList {
-        TODO("Not yet implemented")
-    }
-
+    override suspend fun getUpcomingMovies(): MovieList = dataSource.getUpcomingMovies()
+    override suspend fun getTopRatedMovies(): MovieList = dataSource.getTopRatedMovies()
+    override suspend fun getPopularMovies(): MovieList = dataSource.getPopularMovies()
 }
