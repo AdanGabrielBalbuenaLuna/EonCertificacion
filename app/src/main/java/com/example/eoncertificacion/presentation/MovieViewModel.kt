5a56dcc03959a9d8553478a7527d877bdfb89705
Gabriel <agbalbuenaluna@gmail.com>
2021-07-05 13:47:10 -0500

    Resource MovieViewModel.kt LiveData and ViewModel Dependencies implemented

--- app/src/main/java/com/example/eoncertificacion/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/eoncertificacion/presentation/MovieViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.eoncertificacion.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.eoncertificacion.core.Resource
+import com.example.eoncertificacion.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+
+class MovieViewModel(private val repo: MovieRepository): ViewModel(){
+
+    fun fecthUpcomingMovies() = liveData(Dispatchers.IO){
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(Triple(repo.getUpcomingMovies(),repo.getTopRatedMovies(),repo.getPopularMovies())))
+        } catch (e: Exception) {
+            emit(Resource.Failure(e))
+        }
+    }
+
+}
+
+class MovieViewModelFactory(private val repo: MovieRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
