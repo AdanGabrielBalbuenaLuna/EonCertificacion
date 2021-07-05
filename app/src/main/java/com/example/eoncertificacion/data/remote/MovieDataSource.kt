5b6650f7839cb3bc75135fe65d5f651778a28dfa
Gabriel <agbalbuenaluna@gmail.com>
2021-07-05 13:05:01 -0500

    Data Source

--- app/src/main/java/com/example/eoncertificacion/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/eoncertificacion/data/remote/MovieDataSource.kt
@@ -1,4 +1,18 @@
 package com.example.eoncertificacion.data.remote
 
+import com.example.eoncertificacion.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList{
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList{
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList{
+        return MovieList()
+    }
 }
