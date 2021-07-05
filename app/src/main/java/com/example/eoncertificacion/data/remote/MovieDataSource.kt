a9b5d129eccb84f89ea4f0d4e630c2eda859b467
Gabriel <agbalbuenaluna@gmail.com>
2021-07-05 13:23:34 -0500

    Suspend on MopvieDataSource for server solicitude

--- app/src/main/java/com/example/eoncertificacion/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/eoncertificacion/data/remote/MovieDataSource.kt
@@ -1,18 +1,20 @@
 package com.example.eoncertificacion.data.remote
 
+import com.example.eoncertificacion.application.AppConstants
 import com.example.eoncertificacion.data.model.MovieList
+import com.example.eoncertificacion.repository.WebService
 
-class MovieDataSource {
+class MovieDataSource(private val webService: WebService) {
 
-    fun getUpcomingMovies(): MovieList{
-        return MovieList()
+    suspend fun getUpcomingMovies(): MovieList {
+        return webService.getUpcomingMovies(AppConstants.API_KEY)
     }
 
-    fun getTopRatedMovies(): MovieList{
-        return MovieList()
+    suspend fun getTopRatedMovies(): MovieList {
+        return webService.getTopRatedMovies(AppConstants.API_KEY)
     }
 
-    fun getPopularMovies(): MovieList{
-        return MovieList()
+    suspend fun getPopularMovies(): MovieList {
+        return webService.getPopulardMovies(AppConstants.API_KEY)
     }
 }
