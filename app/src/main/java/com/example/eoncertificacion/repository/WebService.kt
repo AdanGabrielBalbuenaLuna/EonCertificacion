3065e0861bafd7ad4d6ebac78f58edea92d4c412
Gabriel <agbalbuenaluna@gmail.com>
2021-07-05 13:18:02 -0500

    Object Retrofit Created

--- app/src/main/java/com/example/eoncertificacion/repository/WebService.kt
+++ app/src/main/java/com/example/eoncertificacion/repository/WebService.kt
@@ -1,6 +1,10 @@
 package com.example.eoncertificacion.repository
 
+import com.example.eoncertificacion.application.AppConstants
 import com.example.eoncertificacion.data.model.MovieList
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -13,5 +17,16 @@ interface WebService {
 
     @GET("movie/popular")
     suspend fun getPopulardMovies(@Query("api_key") apiKey: String): MovieList
+
+}
+
+object RetrofitClient {
+
+    val webservice by lazy {
+        Retrofit.Builder()
+            .baseUrl(AppConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build().create(WebService::class.java)
+    }
 
 }
